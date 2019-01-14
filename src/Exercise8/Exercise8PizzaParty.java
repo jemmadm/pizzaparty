04d394dffed3b47fd72aa4a75cd61bae23e007bd
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2019-01-14 09:21:28 +0000

    Code comments

--- src/Exercise8/Exercise8PizzaParty.java
+++ src/Exercise8/Exercise8PizzaParty.java
@@ -4,21 +4,14 @@ public class Exercise8PizzaParty {
 
 
     public String pizzaDivider(int numberOfPeople, int numberOfPizzas, int numberOfSlicesPerPizza) {
-        int pizzaCalculator = (numberOfPizzas * numberOfSlicesPerPizza) / numberOfPeople;
+        int numberOfSlicesPerPerson = (numberOfPizzas * numberOfSlicesPerPizza) / numberOfPeople;
 
         int leftOvers = leftoversCalculator(numberOfPeople, numberOfPizzas, numberOfSlicesPerPizza);
 
-
-        return "Each person gets " + pizzaCalculator + " slices and there are " + leftOvers + " slices remaining.";
-
-
+        return "Each person gets " + numberOfSlicesPerPerson + " slices and there are " + leftOvers + " slices remaining.";
     }
 
     public int leftoversCalculator(int numberOfPeople, int numberOfPizzas, int numberOfSlicesPerPizza) {
-        int leftovers = (numberOfPizzas * numberOfSlicesPerPizza) % numberOfPeople;
-
-
-        return leftovers;
-
+        return (numberOfPizzas * numberOfSlicesPerPizza) % numberOfPeople;
     }
 }
