cd3699127d56ec4c7893c2e60b4530d009e42a32
Jemma <jemmamillman@gmail.com>
2019-01-23 10:41:33 +0000

    add Exercise 9

--- src/Exercise9/Exercise9PaintCalculator.java
+++ src/Exercise9/Exercise9PaintCalculator.java
@@ -3,10 +3,15 @@ package Exercise9;
 public class Exercise9PaintCalculator {
     private final static double CONVERSION_FACTOR = 0.002857142857143;
 
-    public String paintNeeded(double width, double length) {
+    public String paintNeededForRectangularRoom(double width, double length) {
         if (length * width % 350 == 0) {
             return "" + (int) (length * width / 350) + " gallons of paint needed";}
          else return  (int)Math.ceil(length * width * CONVERSION_FACTOR) + " gallons of paint needed";
+    }
+
+    public String paintNeededForRoundRoom(double radius){
+
+        return "" + (int)Math.ceil((radius*radius)*Math.PI);
     }
 
 }
--- src/Exercise9/Exercise9PaintCalculatorTest.java
+++ src/Exercise9/Exercise9PaintCalculatorTest.java
@@ -17,57 +17,72 @@ public class Exercise9PaintCalculatorTest {
     @Test
 
     public void noValueForWidth() {
-        assertThat(exercise9PaintCalculator.paintNeeded(0, 3)).isEqualTo("0 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(0, 3)).isEqualTo("0 gallons of paint needed");
     }
 
     @Test
     public void noValueForLength() {
-        assertThat(exercise9PaintCalculator.paintNeeded(5, 0)).isEqualTo("0 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(5, 0)).isEqualTo("0 gallons of paint needed");
     }
 
     @Test
     public void noValues() {
-        assertThat(exercise9PaintCalculator.paintNeeded(0, 0)).isEqualTo("0 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(0, 0)).isEqualTo("0 gallons of paint needed");
     }
 
     @Test
     public void valuesOf1() {
-        assertThat(exercise9PaintCalculator.paintNeeded(1, 1)).isEqualTo("1 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(1, 1)).isEqualTo("1 gallons of paint needed");
     }
 
 
     @Test
     public void areaIs350() {
-        assertThat(exercise9PaintCalculator.paintNeeded(350, 1)).isEqualTo("1 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(350, 1)).isEqualTo("1 gallons of paint needed");
     }
 
     @Test
     public void areaIs700() {
-        assertThat(exercise9PaintCalculator.paintNeeded(350, 2)).isEqualTo("2 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(350, 2)).isEqualTo("2 gallons of paint needed");
     }
 
     @Test
     public void areaIs360() {
-        assertThat(exercise9PaintCalculator.paintNeeded(60, 6)).isEqualTo("2 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(60, 6)).isEqualTo("2 gallons of paint needed");
     }
 
     @Test
     public void areaIs750() {
-        assertThat(exercise9PaintCalculator.paintNeeded(375, 2)).isEqualTo("3 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(375, 2)).isEqualTo("3 gallons of paint needed");
     }
 
     @Test
     public void width349Length1() {
-        assertThat(exercise9PaintCalculator.paintNeeded(349, 1)).isEqualTo("1 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(349, 1)).isEqualTo("1 gallons of paint needed");
     }
 
     @Test
     public void width351Length1() {
-        assertThat(exercise9PaintCalculator.paintNeeded(351, 1)).isEqualTo("2 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(351, 1)).isEqualTo("2 gallons of paint needed");
     }
 
     @Test
     public void width2264Length3() {
-        assertThat(exercise9PaintCalculator.paintNeeded(2263, 3)).isEqualTo("20 gallons of paint needed");
+        assertThat(exercise9PaintCalculator.paintNeededForRectangularRoom(2263, 3)).isEqualTo("20 gallons of paint needed");
+    }
+
+    @Test
+    public void roundRoomRadius0(){
+        assertThat(exercise9PaintCalculator.paintNeededForRoundRoom(0)).isEqualTo("0");
+    }
+
+    @Test
+    public void roundRoomRadius2(){
+        assertThat(exercise9PaintCalculator.paintNeededForRoundRoom(2)).isEqualTo("13");
+    }
+
+    @Test
+    public void roundRoomRadius5(){
+        assertThat(exercise9PaintCalculator.paintNeededForRoundRoom(5)).isEqualTo("79");
     }
 }
