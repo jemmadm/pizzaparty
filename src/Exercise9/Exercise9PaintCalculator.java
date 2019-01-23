3f7c0eeab09bd34111a01c3d7056f6bf705295cf
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2019-01-23 11:59:31 +0000

    Reviewing exercise 9

--- src/Exercise9/Exercise9PaintCalculator.java
+++ src/Exercise9/Exercise9PaintCalculator.java
@@ -1,17 +1,13 @@
 package Exercise9;
 
 public class Exercise9PaintCalculator {
-    private final static double CONVERSION_FACTOR = 0.002857142857143;
+    private final static double SQUARE_FEET_PER_GALLON = 350;
 
     public String paintNeededForRectangularRoom(double width, double length) {
-        if (length * width % 350 == 0) {
-            return "" + (int) (length * width / 350) + " gallons of paint needed";
-        } else return (int) Math.ceil(length * width * CONVERSION_FACTOR) + " gallons of paint needed";
+        return (int) Math.ceil(length * width / SQUARE_FEET_PER_GALLON) + " gallons of paint needed";
     }
 
     public String paintNeededForRoundRoom(double radius) {
-
-
         return "" + (int) Math.ceil((radius * radius) * Math.PI);
     }
 }
